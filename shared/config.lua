34b18cbf3aaac2103ce138c6dcc77e2347383a9a
XSCRIPTS <104461809+xscriptsdev@users.noreply.github.com>
2025-01-25 20:46:26 +0100

    Create config.lua

---
+++ shared/config.lua
@@ -0,0 +1,17 @@
+
+--██╗░░██╗  ░██████╗░█████╗░██████╗░██╗██████╗░████████╗░██████╗
+--╚██╗██╔╝  ██╔════╝██╔══██╗██╔══██╗██║██╔══██╗╚══██╔══╝██╔════╝
+--░╚███╔╝░  ╚█████╗░██║░░╚═╝██████╔╝██║██████╔╝░░░██║░░░╚█████╗░
+--░██╔██╗░  ░╚═══██╗██║░░██╗██╔══██╗██║██╔═══╝░░░░██║░░░░╚═══██╗
+--██╔╝╚██╗  ██████╔╝╚█████╔╝██║░░██║██║██║░░░░░░░░██║░░░██████╔╝
+--╚═╝░░╚═╝  ╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░░░░╚═╝░░░╚═════╝░
+-- Support: https://discord.gg/N74Yuq9ARQ
+
+Config = {}
+
+Config.RequiredItem = "basic_rod" -- Here you can put your own item for fishing rod or use this one
+
+Config.FishingRewards = { "fish", "shark" } -- you can put more if u need to
+
+Config.BreakRodAfterFails = true -- true for to break the road after fails
+Config.FailAttemptsToBreak = 1  -- how many times player need to fail for breaking the rod
